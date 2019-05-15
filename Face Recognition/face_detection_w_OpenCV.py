a09e501c810594907a1e561794c2104d9198cd09
Alexis Leveratto <aleveratto002@soa.ad.pwcinternal.com>
2019-05-15 17:15:17 -0300

    change some code in face detection and add face-det on video

--- Face Recognition/face_detection_w_OpenCV.py
+++ Face Recognition/face_detection_w_OpenCV.py
@@ -4,7 +4,7 @@ import cv2
 
 ap = argparse.ArgumentParser()
 
-ap.add_argument('-i', '--image', required=True, help='path to input image')
+ap.add_argument('-i', '--image', default=[], help='path to input image')
 ap.add_argument('-p', '--prototxt', required=True, help='path to Caffe deploy prototxt file')
 ap.add_argument('-m', '--model', required=True, help='path to Caffe pre-trained model')
 ap.add_argument('-c', '--confidence', type=float, default=0.5, help='minimum probability to filter weak detection') 
@@ -43,10 +43,8 @@ for i in range(0, detections.shape[2]):
 		# probability
 		text = "{:.2f}%".format(confidence * 100)
 		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(image, (startX, startY), (endX, endY),
-			(0, 0, 255), 2)
-		cv2.putText(image, text, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
  
 # show the output image
 cv2.imshow("Output", image)
